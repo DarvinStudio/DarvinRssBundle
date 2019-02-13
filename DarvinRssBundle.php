126cb355869976a5ea07977d0d191c1bd6283c6a
Alexey Gorshkov <moonhorn@example.com>
2019-02-13 09:34:36 +0300

    Create bundle.

---
+++ DarvinRssBundle.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author    Alexey Gorshkov <moonhorn33@gmail.com>
+ * @copyright Copyright (c) 2019, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\RssBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * RSS bundle
+ */
+class DarvinRssBundle extends Bundle
+{
+}
