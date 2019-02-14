7f23207eda8530012e770573dd4555343d3ec7d1
Alexey Gorshkov <moonhorn@example.com>
2019-02-14 10:00:20 +0300

    Add mapper.

---
+++ Mapper/Mapper.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2018, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\RssBundle\Mapper;
+
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
+
+/**
+ * Mapper
+ */
+class Mapper implements MapperInterface
+{
+    /**
+     * @var \Symfony\Component\PropertyAccess\PropertyAccessorInterface
+     */
+    private $propertyAccessor;
+
+    /**
+     * @param \Symfony\Component\PropertyAccess\PropertyAccessorInterface $propertyAccessor Property accessor
+     */
+    public function __construct(PropertyAccessorInterface $propertyAccessor)
+    {
+        $this->propertyAccessor = $propertyAccessor;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function map($source, $target, array $mapping): void
+    {
+        foreach ($mapping as $targetPath => $sourcePath) {
+            if (false === $sourcePath) {
+                continue;
+            }
+            if (null === $sourcePath) {
+                $sourcePath = $targetPath;
+            }
+            if ($this->propertyAccessor->isWritable($target, $targetPath)) {
+                $this->propertyAccessor->setValue($target, $targetPath, $this->propertyAccessor->getValue($source, $sourcePath));
+            }
+        }
+    }
+}
---
+++ Mapper/MapperInterface.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2018, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\RssBundle\Mapper;
+
+/**
+ * Mapper
+ */
+interface MapperInterface
+{
+    /**
+     * @param mixed $source  Source
+     * @param mixed $target  Target
+     * @param array $mapping Mapping
+     */
+    public function map($source, $target, array $mapping): void;
+}
